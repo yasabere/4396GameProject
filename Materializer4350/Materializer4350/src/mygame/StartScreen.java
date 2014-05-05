d163f7057fc058592d014ca36c17b465a80e7e6d
Matt <Matt@10.109.7.119>
2014-05-05 02:11:41 -0400

    Up to Date

--- Materializer4350/Materializer4350/src/mygame/StartScreen.java
+++ Materializer4350/Materializer4350/src/mygame/StartScreen.java
@@ -7,20 +7,59 @@ package mygame;
 import com.jme3.app.Application;
 import com.jme3.app.state.AbstractAppState;
 import com.jme3.app.state.AppStateManager;
+import com.jme3.font.BitmapFont;
+import com.jme3.font.BitmapText;
+import com.jme3.input.InputManager;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.math.ColorRGBA;
+import com.jme3.ui.Picture;
 
 /**
  *
  * @author Nick
  */
-public class StartScreen extends AbstractAppState {
+public class StartScreen extends AbstractAppState implements ActionListener {
     
+    Main main;
+    AppStateManager asm;
+    BitmapText text;
+        
+    public void onAction(String name, boolean isPressed, float tpf) {
+        
+        if (name.equals("start")) {
+            startGame();
+        }
+    }
     
     @Override
     public void initialize(AppStateManager stateManager, Application app) {
+	main = (Main) app;
+        asm = stateManager;
+	//Game game = new Game();
+        //stateManager.attach(game);
+        
+        Picture pic = new Picture("HUD Picture");
+        pic.setImage(main.getAssetManager(), "Interface/resources/background.png", true);
+        pic.setWidth(1024);
+        pic.setHeight(600);
+        pic.setPosition(0, 0);
+        main.getGuiNode().attachChild(pic);
+        //init input
+        InputManager inputManager = main.getInputManager();
+        inputManager.addMapping("start", new KeyTrigger(KeyInput.KEY_B));
+        inputManager.addListener(this, "start"); 
 	
-	Game game = new Game();
-        stateManager.attach(game);
-	
+    }
+    
+    public void startGame() {
+        Game game = new Game();
+        asm.detach(this);
+        main.clearJMonkey(main);
+        asm.attach(game);
+        
+        
     }
     
     @Override
